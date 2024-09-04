238b470d75c3ab5b3f4e206cdcd1146e377d0c0c
quentin.costagliola <costagliola.quentin@gmail.com>
2024-09-04 11:23:10 +0200

    origin to main

--- setup.py
+++ setup.py
@@ -7,22 +7,22 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.13'
-DESCRIPTION = 'Streaming video data via networks'
-LONG_DESCRIPTION = 'A package that allows to build simple streams of video, audio and camera data.'
+VERSION = '0.0.1'
+DESCRIPTION = 'Fall Apart Pipeline'
+LONG_DESCRIPTION = 'A Pipeline for fall apart.'
 
 # Setting up
 setup(
-    name="vidstream",
+    name="fall_apart",
     version=VERSION,
-    author="NeuralNine (Florian Dedov)",
-    author_email="<mail@neuralnine.com>",
+    author="Fall Apart Team",
+    author_email="<fallapart@artfx.fr>",
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     long_description=long_description,
     packages=find_packages(),
-    install_requires=['opencv-python', 'pyautogui', 'pyaudio'],
-    keywords=['python', 'video', 'stream', 'video stream', 'camera stream', 'sockets'],
+    install_requires=['opencv-python', 'pyautogui', 'pyaudio', 'twine', 'setuptools'],
+    keywords=['python', 'vfx', 'pipeline'],
     classifiers=[
         "Development Status :: 1 - Planning",
         "Intended Audience :: Developers",
